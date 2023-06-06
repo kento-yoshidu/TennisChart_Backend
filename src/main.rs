7d7b73f566494e25806b9a6f9ca51e0c7e42cc52
kento <kento@toriwatari.bird>
2023-06-06 22:20:17 +0900

    ⚡ Update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{
+    get, App,
+    web::{Data},
+    Responder, HttpResponse, HttpServer
+};
+
+#[get("/")]
+pub async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("127.0.0.1", 8000))?
+    .run()
+    .await
 }
