2cbc3aaa0725076bda5f282d85a8c0392f700e1a
kento <kento@toriwatari.bird>
2023-06-07 21:52:17 +0900

    ⚡ DBへの接続OK

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,12 @@ use actix_web::{
     web::{Data},
     Responder, HttpResponse, HttpServer
 };
+use dotenv::dotenv;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres, database};
+
+pub struct AppState {
+    db: Pool<Postgres>
+}
 
 #[get("/")]
 pub async fn hello() -> impl Responder {
@@ -11,8 +17,19 @@ pub async fn hello() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    dotenv().ok();
+
+    let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&database_url)
+        .await
+        .expect("Error building a connection");
+
+    HttpServer::new(move || {
         App::new()
+            .app_data(Data::new(AppState { db: pool.clone() }))
             .service(hello)
     })
     .bind(("127.0.0.1", 8000))?
